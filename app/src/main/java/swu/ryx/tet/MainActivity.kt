4dd77c737070ea8ef1538e60397929810cc90cd0
SWURYX <514321690@qq.com>
2020-07-11 14:43:54 +0800

    1.创建并登录

---
+++ app/src/main/java/swu/ryx/tet/MainActivity.kt
@@ -0,0 +1,15 @@
+package swu.ryx.tet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    private fun login(){
+        //登陆
+    }
+}
